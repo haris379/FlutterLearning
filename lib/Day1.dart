fb8c6270dcbf975c2aabb3a140d0bb921ae0dabc
Muhammad Haris <sheikharis574@gmail.com>
2026-09-18 20:34:48 +0500

    First Flutter Project

---
+++ lib/Day1.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Day1 extends StatelessWidget {
+  const Day1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: 250,
+              width: 250,
+              color: Colors.amber,
+              child: Center(
+                child: Text(
+                  "Hello World",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              height: 250,
+              width: 250,
+              color: Colors.red,
+              child: Icon(Icons.favorite, color: Colors.white, size: 40),
+            ),
+
+            Stack(
+              children: [
+                Container(height: 250, width: 250, color: Colors.blue),
+                Container(
+                    margin: EdgeInsets.all(50),
+                    height: 150, width: 150, color: Colors.black),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
