456b4cba140ea3c118902a382eeced8f09f57e78
Muhammad Haris <sheikharis574@gmail.com>
2026-09-19 01:50:17 +0500

    handle check

--- lib/MyText.dart
+++ lib/MyText.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
 
 class Mytext extends StatelessWidget {
-   Mytext({super.key, required this.title, this.size = 20, this.color = Colors.black });
+  Mytext({
+    super.key,
+    required this.title,
+    this.size = 20,
+    this.color = Colors.white,
+    required this.check,
+  });
 
-   String title;
-   double ?size;
-   Color ?color;
+  String title;
+  double? size;
+  Color? color;
+  bool check;
 
   @override
   Widget build(BuildContext context) {
-    return Text(title, style: TextStyle(
+    final textStyle = TextStyle(
       fontSize: size,
       color: color,
       fontWeight: FontWeight.bold,
-    ),);
+    );
+
+    final textPainter = TextPainter(
+      text: TextSpan(text: title, style: textStyle),
+      textDirection: TextDirection.ltr,
+    )..layout();
+
+    return SizedBox(
+      height: textPainter.height,
+      width: textPainter.width,
+      child: Stack(
+        children: [
+          Text(title, style: textStyle),
+          if (check)
+            Positioned(
+              top: textPainter.height / 2 - 1.2, // vertical center, minus half line thickness
+              left: 0,
+              child: Container(
+                height: 2.5,
+                width: textPainter.width,
+                color: color, // exact same color as text
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
