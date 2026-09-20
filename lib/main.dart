b3ea40ae276c51980338c0b29a06f41b2e9d6897
Muhammad Haris <sheikharis574@gmail.com>
2026-09-20 23:23:54 +0500

    Export Day6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Day5.dart';
+import 'Day6.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Day5());
+    return MaterialApp(home: Day6());
   }
 }
