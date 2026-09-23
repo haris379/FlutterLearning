8c8848109e9e567e218fca199f923b36c7cdc7f6
Muhammad Haris <sheikharis574@gmail.com>
2026-09-23 23:37:18 +0500

    Add Slidbar

--- lib/ToDoTile.dart
+++ lib/ToDoTile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simplefirstproject/MyText.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class Todotile extends StatelessWidget {
   Todotile({
@@ -15,29 +16,45 @@ class Todotile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF0D47A1),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        height: 70,
-        width: 415,
-        child: Row(
-          children: [
-            Checkbox(
-              activeColor: Colors.white,
-              checkColor: Colors.black,
-              value: value,
-              onChanged: onChanged,
-              shape: StadiumBorder(side: BorderSide()),
+    return Slidable(
+
+      endActionPane: ActionPane(motion: StretchMotion(), children: [
+        Expanded(
+          child: Container(
+            height: 70,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Color(0xFF2196F3),
+              borderRadius: BorderRadius.circular(30)
             ),
-            Container(
-              margin: EdgeInsets.only(left: 60),
-              child: Mytext(title: text, check: value),
-            ),
-          ],
+          ),
+        )
+      ]),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF0D47A1),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          height: 70,
+          width: 415,
+          child: Row(
+            children: [
+              Checkbox(
+                activeColor: Colors.white,
+                checkColor: Colors.black,
+                value: value,
+                onChanged: onChanged,
+                shape: StadiumBorder(side: BorderSide()),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 60),
+                child: Mytext(title: text, check: value),
+              ),
+              
+            ],
+          ),
         ),
       ),
     );
