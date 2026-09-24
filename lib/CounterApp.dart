be1f5e5fabea0964b7e9917d7011ab31a2772b5c
Muhammad Haris <sheikharis574@gmail.com>
2026-09-24 21:30:04 +0500

    Create new file for counter app

---
+++ lib/CounterApp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Counterapp extends StatefulWidget {
+  const Counterapp({super.key});
+
+  @override
+  State<Counterapp> createState() => _CounterappState();
+}
+
+class _CounterappState extends State<Counterapp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simplefirstproject/CounterApp.dart';
 import 'package:simplefirstproject/ProductCard.dart';
 
 void main() {
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ProductCard());
+    return MaterialApp(home: Counterapp());
   }
 }
