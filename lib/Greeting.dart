3ab98822f65f282137b13c7c762d0e5acf3a3186
Muhammad Haris <sheikharis574@gmail.com>
2026-09-24 23:10:47 +0500

    Create App Bar

--- lib/Greeting.dart
+++ lib/Greeting.dart
@@ -10,6 +10,19 @@ class Greeting extends StatefulWidget {
 class _GreetingState extends State<Greeting> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Greeting App",
+          style: TextStyle(
+            color: Colors.white70,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+    );
   }
 }
