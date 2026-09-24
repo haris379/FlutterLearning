21b24664b30894255325ab37990ee801d1c2c561
Muhammad Haris <sheikharis574@gmail.com>
2026-09-24 21:42:07 +0500

    Create Buttons

--- lib/CounterApp.dart
+++ lib/CounterApp.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
 
 class Counterapp extends StatefulWidget {
-  const Counterapp({super.key});
+  Counterapp({super.key});
 
   @override
   State<Counterapp> createState() => _CounterappState();
 }
 
 class _CounterappState extends State<Counterapp> {
+  int variable = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Center(
+            child: const Text(
+              "Counter App",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.black,
+        ),
+
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(variable.toString(), style: TextStyle(fontSize: 30)),
+              SizedBox(height: 20),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: Text("Increment")),
+                    SizedBox(width: 20),
+                    ElevatedButton(onPressed: () {}, child: Text("Decrement")),
+                    SizedBox(width: 20),
+                    ElevatedButton(onPressed: () {}, child: Text("Reset")),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
